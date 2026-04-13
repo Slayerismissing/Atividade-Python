3ffb961ff12e3300c8d2614d90643bac3d2a385a
Kevin Burgos <158784753+Slayerismissing@users.noreply.github.com>
2026-04-13 11:58:26 -0300

    aula 133

--- udemy/aula_dic.py
+++ udemy/aula_dic.py
@@ -13,17 +13,92 @@ update - Atualiza um dicionário com outro
 
 
 """
+# p1 = {
+#     'nome':'Kevin',
+#     'sobrenome':'Burgos"'
+# }
+
+# # print(p1.get('nome'))
+
+# # nome = p1.pop('nome')
+# # print(nome)
+# # print(p1)
+
+# # nome = p1.popitem()
+# # print(nome)
+# # print(p1)
+
+# # p1.update({
+# #     'nome':'novo valor',
+# #     'idade':45,
+# # })
+
+# # p1.update(nome='novo valor', idade=30)
+
+# tupla = ('nome','novo valor'),
+# p1.update(tupla)
+# print(p1)
+
+perguntas = [
+    {
+        'Pergunta':'Quanto é 2+2?',
+        'Opções':['3','2','5','4'],
+        'Resposta':'4',
+    },
+    {
+        'Pergunta':'Quanto é 5*5?',
+        'Opções':['90','12','25','10'],
+        'Resposta':'25',
+    },{
+        'Pergunta':'Quanto é 10/2?',
+        'Opções':['4','2','5','1'],
+        'Resposta':'5',
+    }
+    ]
 
 
-pessoa = {
-    'nome':"Kevin",
-    'sobrenome':"Burgos",
-}
+qtd_acertos = 0
+for pergunta in perguntas:
+    print('Pergunta',pergunta['Pergunta'])
 
-print(len(pessoa)) # 2
-print(list(pessoa.keys())) # ['nome','sobrenome']
-print(list(pessoa.values())) #['Kevin','Burgos']
-print(list(pessoa.items())) #[('nome','Kevin'), ('sobrenome', 'Burgos')]
+    opcoes = pergunta["Opções"]
+
+    for i, opcao in  enumerate(opcoes):
+        print(f"{i}) ", opcao)
+    print()
+
+
+    escolha = input('Escolha uma opção: ')
+
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+        print('digit true')
+    else:
+        print('Isso não é um dígito')
+        break
+
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True 
+
+    print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou ✔')
+    else:
+        print('Errou ❌')
+
+
+print(f"Você acertou: {qtd_acertos} de {len(perguntas)}")
+
+
+       
+        
 
 
 
